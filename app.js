436b18ed14f502df316018aa0113c64d2236283a
Rushikesh-Naidu <rushikeshnaidu.t@gmail.com>
2023-04-20 23:54:05 +0530

    Created Customer API

--- app.js
+++ app.js
@@ -1,13 +1,98 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Customer = require('./models/customerModel')
 const app = express();
 
+app.use(express.json());
+
 // Routes
-app.get('/customer',(req,res)=>{
-    res.send("Customer API is created successfully...!")
+// GET
+// Get All Customers
+app.get('/customers',async(req,res)=>{ 
+    try {
+        const customer = await Customer.find()
+        res.status(200).json(customer)
+    } catch (error) {
+        res.status(error.status).json({message:error.message})
+    }
 })
+
+// Get Customer By Customer Phone Number
+app.get('/customers/custPhone',async(req,res)=>{ 
+    try {
+        const phone = req.query.phoneNumber;
+        const customer = await Customer.find({"phoneNumber": phone})
+        res.status(200).json(customer)
+    } catch (error) {
+        res.status(error.status).json({message:error.message})
+    }
+})
+
+// Get Customer By Customer ID
+app.get('/customers/:id',async(req,res)=>{ 
+    try {
+        const {id} = req.params;
+        const customer = await Customer.findById(id)
+        res.status(200).json(customer)
+    } catch (error) {
+        res.status(error.status).json({message:error.message})
+    }
+})
+// End of GET api
+
+// POST
+app.post('/customers', async(req,res)=>{
+    try {
+        const customer = await Customer.create(req.body)
+        res.status(201).json(customer);
+    } catch (error) {
+        res.status(error.status).json({message: error.message})
+    }
+})
+// End of POST api
+
+// PUT
+app.put('/customers/:id',async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const customer = await Customer.findByIdAndUpdate(id, req.body);
+        if(!customer){
+            return res.status(404).json({message: `Can not find customer with id ${id}...!`})
+        }
+        const updatedCustomer = await Customer.findById(id);
+        res.status(201).json(updatedCustomer);
+
+    } catch (error) {
+        res.status(error.status).json({message: error.message})
+    }
+})
+// End Of PUT api
+
+// DELETE
+app.delete('/customers/:id',async(req,res)=>{
+    try {
+        const {id} = req.params;
+        const customer = await Customer.findByIdAndDelete(id);
+        if(!customer){
+            return res.status(404).json({message : `Can not find customer with id ${id}...!`});
+        }
+        res.status(201).json(customer);
+    } catch (error) {
+        res.status(error.status).json({message: error.message})
+    }
+})
+// End of DELETE api
 
 
 // APP Listener
 app.listen(1206, ()=>{
     console.log("API is up and running on port 1206");
+})
+mongoose
+.connect('mongodb+srv://rushikesh:doUbiNN8hucvP0yl@mashwara-backend.m54tzni.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("connected to database");
+})
+.catch((error)=>{
+    console.log(error);
 })
---
+++ models/customerModel.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const customerSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please Enter Customer Name...!"]
+        },
+        phoneNumber: {
+            type: String,
+            required: [true],
+        },
+        password: {
+            type: String,
+            required: [true],
+        },
+        email: {
+            type: String,
+            required: [false],
+            default: null
+        },
+        age: {
+            type: Number,
+            required: [false],
+            default: null
+        },
+        gender: {
+            type: Number, //Gender Value 1 For Male, 2 For Female, 3 For Others
+            required: [false],
+            default: null
+        },
+        diet: {
+            type: String,
+            required: [false],
+            default: "ANY"
+        },
+        profilePic: {
+            type: String,
+            required: [false],
+            default: null
+        }
+    },
+    {
+        timestamps : true
+    }
+)
+
+
+const Customer = mongoose.model('Customer',customerSchema);
+
+module.exports = Customer;
